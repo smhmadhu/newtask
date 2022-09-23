a92c30622deafaf6331cec054440002f411429ce
smhmadhu <smhmadhu@gmail.com>
2022-09-23 11:59:01 +0530

    super

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,33 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOMR() {
+		System.out.println("OMRBRANCH");
+	}
+
+	private void greensAdayar() {
+		System.out.println("ADAYARBRANCH");
+	}
+
+	private void greensTambaram() {
+		System.out.println("TAMBARAMBRANCH");
+	}
+
+	private void greensvelachery() {
+		System.out.println("VELACHERYBRANCH");
+
+	}
+
+	private void greensAnnanagar() {
+		System.out.println("ANNANAGAR");
+	}
+
+	public static void main(String[] args) {
+		GreensTech a = new GreensTech();
+		a.greensOMR();
+		a.greensAdayar();
+		a.greensTambaram();
+		a.greensvelachery();
+		a.greensAnnanagar();
+	}
+}
